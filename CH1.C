7474d7e7d4c9c18295ff680ad83f568607b9d70e
Yash Ratewal <yashratewal2007@gmai.com>
2026-09-09 21:35:11 +0530

    Added CH1.C

---
+++ CH1.C
@@ -0,0 +1,66 @@
+// hello world program
+//  #include <stdio.h>
+// int main() {
+//     printf("hello world\n");
+//     return 0;   
+// } 
+
+// datatypes in c 
+//  #include <stdio.h>
+//  int main() {
+//     int a = 10; // integer data type 
+//     float b = 10.5; // float data type 
+//     char c = 'A'; // character data type 
+
+//      printf("a is %d\n", a);
+//      printf("b is %f\n", b); 
+//      printf("c is %c\n",c);
+
+//      return 0;
+//  }
+
+
+ //sum of two numbers 
+//  #include <stdio.h>
+//  int main() {
+//     int a, b;
+//     printf("enter a \n");
+//     scanf("%d", &a); // taking input from user
+//     printf("enter b \n");
+//     scanf("%d", &b); // taking input from user
+
+//     printf("sum is %d\n", a + b);
+//     return 0;
+//  } 
+
+
+ // Area of Square
+ /* #include <stdio.h>
+ int main() {
+    int side;
+    printf("enter side of square \n");
+    scanf("%d", &side); // taking input from user
+    printf("area of square is %d\n", side*side);
+    return 0;
+ } */ 
+
+ //Area of circle 
+ /* #include <stdio.h>
+ int main() {
+    float radius;
+    scanf("%f", &radius); // taking input from user
+    printf("area of circle is %f\n", 3.14*radius*radius);
+    return 0;
+ } */
+
+// short introduction about myself
+/*  #include <stdio.h>
+ int main() {
+    printf("My name is Yash \n");
+    printf("I am learning C programming language \n");
+    printf("i will become a very successful businessman in future\n");
+    return 0;
+ } */
+ 
+ 
+
